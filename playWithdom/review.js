d68f99393b96ac6350c0c4eddfdc4b64b69911ae
Musab Al Mahi <musabalmahi53@gmail.com>
2024-03-09 23:14:41 +0600

    when i press enter key in my goriber evally project then it will be showed front side

--- playWithdom/review.js
+++ playWithdom/review.js
@@ -1,33 +1,34 @@
 document.querySelector("#submit-btn").addEventListener("click", function () {
-    //   console.log("clicked");
-  
+  //   console.log("clicked");
+
+  const textAreaElement = document.getElementById("text-area");
+  const areaValue = textAreaElement.value;
+  //   console.log(textAreaElement.value);
+
+  const p = document.createElement("p");
+  p.innerText = areaValue;
+  //   console.log(p);
+
+  const container = document.querySelector("#review");
+  container.appendChild(p);
+  document.getElementById("text-area").value = "";
+});
+
+//   keyboard er enter a chap dile jeno eki kaj hoy click er motto sey code ekhan theke suru
+document.getElementById("text-area").addEventListener("keyup", function (e) {
+  console.log(e.target.value);
+  if (e.key === "Enter") {
+    // console.log("Enter key pressed!");
     const textAreaElement = document.getElementById("text-area");
     const areaValue = textAreaElement.value;
     //   console.log(textAreaElement.value);
-  
+
     const p = document.createElement("p");
     p.innerText = areaValue;
     //   console.log(p);
-  
+
     const container = document.querySelector("#review");
     container.appendChild(p);
     document.getElementById("text-area").value = "";
-  });
-  
-  document.getElementById("text-area").addEventListener("keyup", function (e) {
-    console.log(e.target.value);
-    if (e.key === "Enter") {
-      // console.log("Enter key pressed!");
-      const textAreaElement = document.getElementById("text-area");
-      const areaValue = textAreaElement.value;
-      //   console.log(textAreaElement.value);
-  
-      const p = document.createElement("p");
-      p.innerText = areaValue;
-      //   console.log(p);
-  
-      const container = document.querySelector("#review");
-      container.appendChild(p);
-      document.getElementById("text-area").value = "";
-    }
-  });
+  }
+});
