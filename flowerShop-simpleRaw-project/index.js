da52cb050d22859cf7728bbe6cf15e7b25334689
Musab Al Mahi <musabalmahi53@gmail.com>
2024-03-10 23:07:29 +0600

    basic functionality exploring done after yet

--- flowerShop-simpleRaw-project/index.js
+++ flowerShop-simpleRaw-project/index.js
@@ -6,6 +6,7 @@ function handleCLikBtn(target) {
   const li = document.createElement("li");
   li.innerText = itemName;
   selectedItemContainer.appendChild(li);
+  
   const price = target.parentNode.childNodes[5].innerText.split(" ")[1];
   total = parseInt(total) + parseInt(price);
   document.getElementById("total").innerText = total;
