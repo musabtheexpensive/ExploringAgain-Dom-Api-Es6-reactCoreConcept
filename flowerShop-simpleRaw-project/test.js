df6ac5a8bb377b0cfb8c5d62eca433ef04d5615b
Musab Al Mahi <musabalmahi53@gmail.com>
2024-03-10 19:14:52 +0600

    exploring another small and tiny project flower shop

---
+++ flowerShop-simpleRaw-project/test.js
@@ -0,0 +1,14 @@
+// function handleClick(data) {
+//   console.log(data.parentNode.childNodes[1].innerText);
+// }
+
+
+function test(params){
+  const sum=toNumber("input1");
+  console.log(sum);
+}
+
+function toNumber(id){
+  const result = document.getElementById(id).value;
+  return parseInt(result);
+}
