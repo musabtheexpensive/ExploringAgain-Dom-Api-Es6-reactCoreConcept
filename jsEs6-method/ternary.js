a0f6fd38300c31c053447bfa77d108b95d885e70
Musab Al Mahi <musabalmahi53@gmail.com>
2024-03-13 20:23:27 +0600

    ternary is  talking in es6

---
+++ jsEs6-method/ternary.js
@@ -0,0 +1,14 @@
+const x = 2;
+
+// if(x>5){
+//     console.log("x is greater than 5")
+// }else{
+//     console.log("x is less than 5")
+// }
+
+// ternary es6 here
+x > 5
+    ?
+    console.log("x is greater than 5")
+    :
+    console.log("x is less than 5")
